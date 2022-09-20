768088e7f1e68c66a0099da113712533b690b494
Srijan Dubey <87586713+Srijan-D@users.noreply.github.com>
2022-09-20 20:02:41 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+
+const { google } = require('googleapis');
+
+
+const app = express();
+
+const port = 3000;
+app.use(urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.send("working")
+})
+
+app.post('/', (req, res) => {
+    let auth = new google.auth.GoogleAuth({
+        keyFile: "credentials.json",
+        scopes: "https://www.googleapis.com/auth/spreadsheets "
+    })
+})
+async function(auth) {
+    const sheets = google.sheets({ version: 'v4', auth })
+    const 
+
+}
+
+
+app.listen({ port }, (req, res) => {
+    console.log("server is running on port 3000")
+})
